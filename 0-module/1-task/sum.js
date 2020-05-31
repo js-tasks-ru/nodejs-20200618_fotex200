9952d4c90cc67cfdc9efe0c99d8777462ad18e59
Artem Pushkarev <apushkarev@MacBook-Pro.local>
2020-05-31 18:44:07 -0400

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+  let checkNumber = [...arguments].find(val => {
+    return (isNaN(val) || typeof val === 'string'); 
+  });
+
+  if (!checkNumber) {
+    return a + b;
+  } else {
+    throw new TypeError('Not a number')
+  };
 }
 
 module.exports = sum;
